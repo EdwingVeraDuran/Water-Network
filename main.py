3f02e1a2914c36f32cffb9475eb373df736ded29
Edwing Vera <edwingveraduran@gmail.com>
2025-09-25 05:08:22 -0500

    Main file were all modules are connected and working together

---
+++ main.py
@@ -0,0 +1,13 @@
+from core.dxf_parser import DXFParser
+from core.graph_builder import GraphBuilder
+from core.visualizer import Visualizer
+
+if __name__ == "__main__":
+    parser = DXFParser("data/prueba.dxf")
+    nodes = parser.parse_nodes()
+    edges = parser.parse_edges()
+    
+    builder = GraphBuilder(nodes, edges)
+    G = builder.build()
+    
+    Visualizer.plot(G)
