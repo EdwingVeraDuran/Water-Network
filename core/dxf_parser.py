7d0e72a8317c7f2d3c6930877428aa9066344296
Edwing Vera <edwingveraduran@gmail.com>
2025-09-25 12:12:26 -0500

    :sparkles: Implement NodeType feature into node parser. Also added naming based on NodeType.

--- core/dxf_parser.py
+++ core/dxf_parser.py
@@ -1,12 +1,13 @@
 import ezdxf
 from models.node import Node
 from models.edge import Edge
+from models.node_type import NodeType
 
-BLOCK_MAP: map = {
-    "APARATO": "aparato",
-    "VALVULA": "valvula",
-    "ACOMETIDA": "acometida",
-    "ACCESORIO": "accesorio",
+BLOCK_MAP: dict[str, NodeType] = {
+    "APARATO": NodeType.APARATO,
+    "VALVULA": NodeType.VALVULA,
+    "ACOMETIDA": NodeType.ACOMETIDA,
+    "ACCESORIO": NodeType.ACCESORIO,
 }
 
 class DXFParser:
@@ -20,16 +21,31 @@ class DXFParser:
     def parse_nodes(self):
         node_id = 1
         
+        val_id = 1
+        apa_id = 1
+        acc_id = 1
+        
         for insert in self.msp.query("INSERT"):
             x, y = insert.dxf.insert.x, insert.dxf.insert.y
             block_name = insert.dxf.name.upper()
             
             node_type = BLOCK_MAP.get(block_name)
             
-            node: Node = Node(id=f"N{node_id}", pos=(x, y), node_type=node_type,)
-            self.nodes.append(node)
-            node_id += 1
+            if (node_type == NodeType.VALVULA):
+                node: Node = Node(id=f"V{val_id}", pos=(x, y), node_type=node_type)
+                val_id += 1
+            elif (node_type == NodeType.APARATO):
+                node: Node = Node(id=f"AP{apa_id}", pos=(x, y), node_type=node_type)    
+                apa_id += 1
+            elif (node_type == NodeType.ACCESORIO):
+                node: Node = Node(id=f"AC{acc_id}", pos=(x, y), node_type=node_type)
+                acc_id += 1
+            else:
+                node: Node = Node(id=f"N{node_id}", pos=(x, y), node_type=node_type,)
+                node_id += 1
         
+            self.nodes.append(node)
+            
         return self.nodes
     
     def find_node_by_coords(self, coord: tuple):
